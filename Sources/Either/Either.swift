4da5d2378e0074e5421f8ec74eb0fd877633e1e0
Georgi Kuklev <gkuklev98@gmail.com>
2024-07-10 23:22:23 +0300

    Make Either public

--- Sources/Either/Either.swift
+++ Sources/Either/Either.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-enum Either<Left, Right> {
+public enum Either<Left, Right> {
     case left(Left)
     case right(Right)
 }
