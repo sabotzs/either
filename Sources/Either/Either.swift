2846049928d7e43dd7d072c1b82407d84cdc8703
Georgi Kuklev <gkuklev98@gmail.com>
2024-07-11 01:02:02 +0300

    Update comment header

--- Sources/Either/Either.swift
+++ Sources/Either/Either.swift
@@ -1,5 +1,6 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+// Either.swift
+// Georgi Kuklev on 09.07.2024
 
 public enum Either<Left, Right> {
     case left(Left)
