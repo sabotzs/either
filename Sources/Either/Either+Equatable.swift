bac919af558e161ccc08df5fccf02eea90ae6552
Georgi Kuklev <gkuklev98@gmail.com>
2024-07-11 01:03:45 +0300

    Add Equatable implementation and tests for it.

---
+++ Sources/Either/Either+Equatable.swift
@@ -0,0 +1,16 @@
+//
+// Either+Equatable.swift
+// Georgi Kuklev on 11.07.2024
+
+extension Either: Equatable where Left: Equatable, Right: Equatable {
+    public static func == (_ lhs: Self, _ rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case let (.left(l), .left(r)):
+            return l == r
+        case let (.right(l), .right(r)):
+            return l == r
+        default:
+            return false
+        }
+    }
+}
---
+++ Tests/EitherTests/EitherEquatableTests.swift
@@ -0,0 +1,39 @@
+//
+// EitherEquatableTests.swift
+// Georgi Kuklev on 11.07.2024
+
+import Testing
+@testable import Either
+
+@Suite("Either+Equatable")
+struct EitherEquatableTests {
+    @Test func sameLeftValuesAreEqual() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .left("hello")
+        #expect(left == right)
+    }
+
+    @Test func differentLeftValuesAreNotEqual() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .left("Hello")
+        #expect(left != right)
+    }
+
+    @Test func sameRightValuesAreEqual() async throws {
+        let left: Either<String, Int> = .right(10)
+        let right: Either<String, Int> = .right(10)
+        #expect(left == right)
+    }
+
+    @Test func differentRightValuesAreNotEqual() async throws {
+        let left: Either<String, Int> = .right(10)
+        let right: Either<String, Int> = .right(100)
+        #expect(left != right)
+    }
+
+    @Test func differentCasesAreNotEqual() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .right(100)
+        #expect(left != right)
+    }
+}
