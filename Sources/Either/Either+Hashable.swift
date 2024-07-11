b9a6b808b107035c2635798001eb3215817bf07d
Georgi Kuklev <gkuklev98@gmail.com>
2024-07-11 23:45:47 +0300

    Add Hashable conformance and tests for it.

---
+++ Sources/Either/Either+Hashable.swift
@@ -0,0 +1,16 @@
+//
+// Either+Hashable.swift
+// Georgi Kuklev on 11.07.2024
+
+extension Either: Hashable where Left: Hashable, Right: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case let .left(left):
+            hasher.combine("left") // Add "left" to represent left
+            hasher.combine(left)
+        case let .right(right):
+            hasher.combine("right") // Add "right" to represent right
+            hasher.combine(right)
+        }
+    }
+}
---
+++ Tests/EitherTests/EitherHashableTests.swift
@@ -0,0 +1,45 @@
+//
+// EitherHashableTests.swift
+// Georgi Kuklev on 11.07.2024
+
+import Testing
+@testable import Either
+
+@Suite("Hash")
+struct EitherHashableTests {
+    @Test func sameLeftValuesHaveSameHashValues() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .left("hello")
+        #expect(left.hashValue == right.hashValue)
+    }
+
+    @Test func differentLeftValuesHaveDifferentHashValues() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .left("Hello")
+        #expect(left.hashValue != right.hashValue)
+    }
+
+    @Test func sameRightValuesHaveSameHashValue() async throws {
+        let left: Either<String, Int> = .right(1011)
+        let right: Either<String, Int> = .right(1011)
+        #expect(left.hashValue == right.hashValue)
+    }
+
+    @Test func differentRightValuesHaveDifferentHashValues() async throws {
+        let left: Either<String, Int> = .right(1011)
+        let right: Either<String, Int> = .right(1010)
+        #expect(left.hashValue != right.hashValue)
+    }
+
+    @Test func differentCasesWithSameValuesHaveDifferentValues() async throws {
+        let left: Either<Int, Int> = .left(1011)
+        let right: Either<Int, Int> = .right(1011)
+        #expect(left.hashValue != right.hashValue)
+    }
+
+    @Test func differentCasesWithDifferentValuesHaveDifferentHashValues() async throws {
+        let left: Either<String, Int> = .left("hello")
+        let right: Either<String, Int> = .right(1011)
+        #expect(left.hashValue != right.hashValue)
+    }
+}
